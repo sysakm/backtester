9ab3bf2b364a3fe5d640b6459f1687eae40c282b
sysakm <sysak-ma@yandex.ru>
2025-12-13 17:13:30 +0200

    Add flag for absolute units spread

--- backtest.py
+++ backtest.py
@@ -2,13 +2,14 @@ import numpy as np
 import pandas as pd
 
 
-def backtester(price_df, signal_df, spread=0):
+def backtester(price_df, signal_df, spread=0, spread_is_relative=True):
     """
     Backtester prototype
     Execution strategy: on the first non-zero signal open position until signal sign switch. On signal sign switch open the inverse position instead.
     price_df: DataFrame ('t', 'price')
     signal_df: DataFrame ('t', 'signal')
     spread: scalar value for constant spread OR DataFrame ('t', 'quoted_spread')
+    spread_is_relative: bool flag, default True, if False divide spread values by respective price
     """
     # Merge prices with signals under condition that the signal timestamp is strictly lower than price timestamp.
     merged_df = pd.merge_asof(price_df, signal_df, on='t', allow_exact_matches=False, direction='backward')
@@ -19,13 +20,15 @@ def backtester(price_df, signal_df, spread=0):
         merged_df['quoted_spread'].fillna(0, inplace=True)
     else:
         merged_df['quoted_spread'] = spread
+    if not spread_is_relative:
+        merged_df['quoted_spread'] = merged_df['quoted_spread'] / merged_df['price']
 
     merged_df['position'] = np.sign(merged_df['signal']).replace(0, np.nan).ffill().fillna(0)
     merged_df['trade'] = merged_df['position'].diff(1).fillna(0)
     # When analyzing return/equity values use positions that created those returns, thus the ones from the prev. bar
     merged_df['r_position'] = merged_df['position'].shift(1).fillna(0)
     
-    merged_df['return'] = (merged_df['r_position'] * (merged_df['price'].diff(1) / merged_df['price'].shift(1))).fillna(0)
+    merged_df['return'] = (merged_df['r_position'] * merged_df['price'].pct_change()).fillna(0)
     merged_df['spread_return'] = np.abs(merged_df['trade']) * merged_df['quoted_spread'] / 2
     merged_df['net_return'] = merged_df['return'] - merged_df['spread_return']
     merged_df['equity'] = (1 + merged_df['net_return']).cumprod()
